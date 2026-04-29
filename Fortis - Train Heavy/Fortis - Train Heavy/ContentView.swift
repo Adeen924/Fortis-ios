ac65521aca84a3db3e503753467b3a7fce3baf5a
AmazloomFiyr <adibmazloom@gmail.com>
2026-04-29 13:54:29 -0700

    New Updated App

---
+++ Fortis - Train Heavy/Fortis - Train Heavy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fortis - Train Heavy
+//
+//  Created by FiyrPod Support on 4/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
