656eb17190af8ce50bcd2b2187a33c9b1916f7da
Adeen924 <adibmazloom@gmail.com>
2026-04-29 22:12:02 -0700

    Update HomeView.swift

--- Fortis - Train Heavy/Fortis - Train Heavy/Views/Home/HomeView.swift
+++ Fortis - Train Heavy/Fortis - Train Heavy/Views/Home/HomeView.swift
@@ -149,8 +149,8 @@ struct HomeView: View {
                     secondaryMuscles: combinedWeeklySecondaryMuscles
                 )
                 .frame(height: 200)
-                .padding(.top, 20)
-                .padding(.bottom, 20)
+                .padding(.top, 35)
+                .padding(.bottom, 35)
             }
         }
     }
